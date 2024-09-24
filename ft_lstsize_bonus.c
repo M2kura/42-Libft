ef15ad4cf200dd5abd2214d8c7d1954096d292f9
M2kura <teterheo@fel.cvut.cz>
2024-09-24 12:04:11 +0200

    Add bonus part functions

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -10,3 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+
+	count = 0;
+	while (lst)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
